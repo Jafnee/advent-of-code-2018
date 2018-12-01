1d11836daa48ca7b21509dc9db5efd159924594c
Jafnee <jafnee@gmail.com>
2018-12-01 18:43:34 +0800

    Solve day 1

---
+++ dayone.py
@@ -0,0 +1,19 @@
+from itertools import cycle
+
+
+with open('dayone-input.txt', 'r') as input_file:
+    numbers = [int(num) for num in input_file]
+# pt 1
+print(sum(numbers))
+
+occurred_frequencies = {0}
+current = 0
+for number in cycle(numbers):
+    current += number
+    already_occured = current in occurred_frequencies
+    if already_occured:
+        # pt 2
+        print(current)
+        break
+    occurred_frequencies.add(current)
+
