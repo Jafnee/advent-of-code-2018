1705ccc40e1e5a45b3debacd2abf996e2a1351e0
Jafnee <jafnee@gmail.com>
2018-12-02 14:23:42 +0800

    Solve day 2 using node.js

--- dayone.js
+++ dayone.js
@@ -1,30 +1,30 @@
 const fs = require('fs')
 
+
 const numbers = fs.readFileSync('dayone-input.txt', 'utf8')
+  .trim()
   .split('\n')
-  .map(x => Number.parseInt(x, 10))
-  // NaN for last blank line in input file.
-  .filter(x => !Number.isNaN(x))
+  .map(Number)
 
 // pt 1
 const sum = numbers.reduce((sum, x) => sum + x, 0)
 console.log(sum)
 
 
-function* numbers_repeating() {
+function* numberRepeating() {
   while (true) {
     yield* numbers
   }
 }
 
-const occurred_frequencies = {'0': 1}
+const occurredFrequencies = {'0': 1}
 let current = 0
-for (let number of numbers_repeating()) {
+for (let number of numberRepeating()) {
   current += number
-  if (occurred_frequencies[current]) {
+  if (occurredFrequencies[current]) {
     // pt 2
     console.log(current)
     break
   }
-  occurred_frequencies[current] = 1
+  occurredFrequencies[current] = 1
 }
---
+++ daytwo.js
@@ -0,0 +1,50 @@
+const fs = require('fs')
+
+const ids = fs.readFileSync('daytwo-input.txt', 'utf8')
+  .trim()
+  .split('\n')
+
+// Array of obj {char: number of times found}
+const occurrencesCounters = ids.map(id => {
+  const asArray = id.split('')
+  const charCount = asArray.reduce((counter, char) => {
+    counter[char] = (counter[char] || 0) + 1
+    return counter
+  }, {})
+  return charCount
+})
+
+const occurrencesValues = occurrencesCounters.map(Object.values)
+
+const [doubleCount, tripleCount] = occurrencesValues.reduce(([double, triple], occurrences) => {
+    if (occurrences.includes(2)) double += 1
+    if (occurrences.includes(3)) triple += 1
+    return [double, triple]
+  }, [0, 0])
+// pt 1
+console.log(doubleCount * tripleCount)
+
+
+const idsAndToCompare = ids.map((id, index) => {
+  // Find what ids to compare this one against
+  const idsToCompare = ids.slice(index + 1)
+  return [id, idsToCompare]
+})
+
+for (let [id, idsToCompare] of idsAndToCompare) {
+  for (otherId of idsToCompare) {
+    // Get char diffs against current id
+    const diff = otherId.split('').reduce((acc, char, i) => {
+      if (char !== id[i]) acc.push(i)
+      return acc
+    }, [])
+
+    if (diff.length === 1) {
+      const diffIndex = diff[0]
+      // pt 2
+      // Rebuild string excluding the diff char
+      console.log(id.slice(0, diffIndex) + id.slice(diffIndex + 1))
+      break
+    }
+  }
+}
