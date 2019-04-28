b234af808eb02c6498247fe02b533f50c31b095d
Jafnee <jafnee@gmail.com>
2019-04-28 22:12:49 +0800

    Optimise day09 Python solution * Use deque instead of list

--- day09.py
+++ day09.py
@@ -1,36 +1,21 @@
-""" TODO optimise
-Swap out costly list insert and pops with a deque.
-Takes a million years to run pt 2 otherwise.
-"""
-
 from itertools import cycle
+from collections import deque
 
 
 def pt_1(players=7, last_marble=25):
     # py3.7 ordered guaranteed
     players = range(1, players + 1)
     scores = {player: 0 for player in players}
-    marbles = [0]
-    current_marble_index = 0
+    marbles = deque([0])
 
     for marble, player in zip(range(1, last_marble + 1), cycle(players)):
         if marble % 23 == 0:
             # Remove from circle & add to score
-            pop_index = (current_marble_index - 7) % len(marbles)
-
-            # Add score to player
-            score_earned = marble + marbles.pop(pop_index)
-            scores[player] += score_earned
-
-            # Marble to the right is now current marble
-            current_marble_index = pop_index
+            marbles.rotate(-7)
+            scores[player] += marble + marbles.pop()
         else:
-            insert_index = (current_marble_index + 2) % len(marbles)
-            # Append to list instead of begginning
-            if not insert_index:
-                insert_index = len(marbles)
-            marbles.insert(insert_index, marble)
-            current_marble_index = insert_index
+            marbles.rotate(2)
+            marbles.append(marble)
     return max(scores.values())
 
 
