f55d76922e6eb5864437ffcdbe935d47b6b64664
Jafnee <jafnee@gmail.com>
2019-05-19 16:45:58 +0800

    Optimise day09.js using deque instead of array
    * Old array implementation took 11049.29s to completed
    * Now takes ~5s
    * Only implemented just enough deque functionality for this challenge.

--- day09.js
+++ day09.js
@@ -1,30 +1,90 @@
-// This is extremely slow to complete pt2.
-// Will need to redo with a doubly linked list implementation for a
-// significant speed up.
-
 const assert = require('assert')
 // JS mod operator is scuffed, doesn't do neg numbers.
 const mod = (n, m) => ((n % m) + m) % m;
 
-function pt1(players=7, lastMarble=25) {
-    const marbles = [0]
+
+class Node {
+    // Doubly Linked List Node
+    next = null
+    prev = null
+
+    constructor(value) {
+        this.value = value
+    }
+}
+
+
+class Deque {
+    // Inspired by Python's collections.deque
+    currentNode = null
+
+    constructor(vals=[]) {
+        const {length} = vals
+        const nodes = vals.map(val => new Node(val))
+        nodes.forEach((node, i)=> {
+            // First one
+            if (!this.currentNode) {
+                this.currentNode = node.next = node.prev = node
+                return
+            }
+            node.prev = this.currentNode
+
+            this.currentNode.next = node
+            this.currentNode = node
+        })
+        // Circle it around
+        this.currentNode.next = nodes[0]
+    }
+
+    append(val) {
+        const newNode = new Node(val)
+        newNode.prev = this.currentNode
+        newNode.next = this.currentNode.next
+        this.currentNode.next.prev = newNode
+        this.currentNode.next = newNode
+        this.currentNode = newNode
+    }
+
+    pop() {
+        this.currentNode.next.prev = this.currentNode.prev
+        this.currentNode.prev.next = this.currentNode.next
+        const popped = this.currentNode.value
+        this.currentNode = this.currentNode.prev
+        return popped
+    }
+
+    rotate(n) {
+        // Rotate forwards for positive numbers, backwards for negative.
+        const prop = n < 0 ? 'next' : 'prev'
+        for (let i = 0; i < Math.abs(n); i++) {
+            this.currentNode = this.currentNode[prop]
+        }
+    }
+
+    // Useful for visualizing
+    toArray() {
+        let next = this.currentNode.next
+        const ret = []
+        do {
+            ret.push(next.value)
+            next = next.next
+        } while (next !== this.currentNode.next)
+        return ret
+    }
+}
+
+function pt1(players, lastMarble) {
+    const marbles =  new Deque([0])
     const scores = new Array(players).fill(0)
-    let currMarbleIdx = 0
 
     for (let marble=1; marble<=lastMarble; marble++) {
         const currPlayerIdx = (marble) % players
         if (marble % 23 === 0) {
-            const popIdx = mod(currMarbleIdx - 7, marbles.length)
-            // Remove marble and add up score for turn
-            const scoreEarned = marbles.splice(popIdx, 1)[0] + marble
-            scores[currPlayerIdx] += scoreEarned
-
-            // Marble to the right of popped is now current marble
-            currMarbleIdx = popIdx
+            marbles.rotate(-7)
+            scores[currPlayerIdx] += marble + marbles.pop()
         } else {
-            let insertIdx = (currMarbleIdx + 2) % marbles.length
-            marbles.splice(insertIdx, 0, marble)
-            currMarbleIdx = insertIdx
+            marbles.rotate(2)
+            marbles.append(marble)
         }
     }
     return Math.max(...scores)
